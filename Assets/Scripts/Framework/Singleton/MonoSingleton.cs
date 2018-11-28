3da748b60d1a8a60334e4f542e25fc5c3b45b5cb
unknown <enigma.twj@hz.ali.com>
2018-11-28 15:38:15 +0800

    删除Classic_ECS_JobSystem 新增Singleton 升级到Unity2018.2.5f1

---
+++ Assets/Scripts/Framework/Singleton/MonoSingleton.cs
@@ -0,0 +1,73 @@
+﻿/******************************************************************************
+ * DESCRIPTION: Mono单例
+ * 
+ *     Copyright (c) 2018, 谭伟俊 (TanWeijun)
+ *     All rights reserved
+ * 
+ * COMPANY:
+ * CREATED: 2018.11.28, 15:00, CST
+*******************************************************************************/
+
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    #region Variables
+    private static T m_Instance;
+    private static bool m_ApplicationIsQuitting;
+
+#if THREAD_SAFE
+    private static object m_Lock = new object();
+#endif
+    #endregion
+
+    #region Properties
+    public static T Instance
+    {
+        get
+        {
+            if (m_ApplicationIsQuitting && (null == m_Instance))
+            {
+                Debug.LogWarning(string.Format("应用程序正在退出，Mono单例（{0}）已销毁.", typeof(T)));
+                return null;
+            }
+
+        #if THREAD_SAFE
+            lock (m_Lock)
+            {
+        #endif
+                if (null == m_Instance)
+                {
+                    var instances = FindObjectsOfType<T>();
+                    if ((null != instances) && (instances.Length > 0))
+                    {
+                        m_Instance = instances[0];
+                        if (instances.Length > 1)
+                        {
+                            Debug.LogError(string.Format("Mono单例（{0}）有多个存在.", typeof(T)));
+                        }
+
+                        return m_Instance;
+                    }
+
+                    var singleton = new GameObject("(singleton) " + typeof(T).Name);
+                    DontDestroyOnLoad(singleton);
+                    
+                    m_Instance = singleton.AddComponent<T>();
+                }
+        #if THREAD_SAFE
+            }
+        #endif
+            
+            return m_Instance;
+        }
+    }
+    #endregion
+
+    #region Unity
+    private void OnDestroy()
+    {
+        m_ApplicationIsQuitting = true;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Framework/Singleton/Singleton.cs
@@ -0,0 +1,42 @@
+﻿/******************************************************************************
+ * DESCRIPTION: 单例
+ * 
+ *     Copyright (c) 2018, 谭伟俊 (TanWeijun)
+ *     All rights reserved
+ * 
+ * COMPANY:
+ * CREATED: 2018.10.16, 19:36, CST
+*******************************************************************************/
+
+public class Singleton<T> where T : new()
+{
+    #region Variables
+    private static T m_Instance;
+    
+#if THREAD_SAFE
+    private static object m_Lock = new object();
+#endif
+    #endregion
+    
+    #region Methods
+    public static T Instance
+    {
+        get
+        {
+        #if THREAD_SAFE
+            lock (m_Lock)
+            {
+        #endif
+                if (null == m_Instance)
+                {
+                    m_Instance = new T();
+                }
+        #if THREAD_SAFE
+            }
+        #endif
+
+            return m_Instance;
+        }
+    }
+    #endregion
+}
